d6de53f635bcc8f71716dcb28cc83dc1cfe9d600
slava-1969 <slava.euro.balkans.transfer@gmail.com>
2026-07-23 21:06:42 +0300

    + fix build

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -1,9 +1,12 @@
 import ThankYou from "@/components/thank-you/thank-you";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    <main>
-      <ThankYou />
-    </main>
+    <Suspense fallback={null}>
+      <main>
+        <ThankYou />
+      </main>
+    </Suspense>
   )
 }
